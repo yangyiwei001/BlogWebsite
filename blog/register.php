6e048fd76f5f7f1208eeb2a129dc4a5bbfd5f0c5
Aaron <841968701@qq.com>
2018-05-13 19:14:30 +0800

    Signed-off-by: Aaron <841968701@qq.com>

--- blog/register.php
+++ blog/register.php
@@ -3,36 +3,34 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 		<title>注册</title>
-		<link rel = "stylesheet" href = "css/register.css">
 	</head>
-	<body background="img/1.png">
-		<div id="content">
+	<body>
+		<div style="border:2px solid #666666;margin:250px auto;width:400px;height:280px;background-color:#CCCCCC">
 			<div align="center">
 				<h1>注册账号</h1>
 			</div>
 			<div align="center">
 				<form action="registeraction.php" method="post">
 					<table>
-						<tr height="40">
-							<td style="font-size:20px;">用&nbsp;户&nbsp;名：</td>
-							<td><input type="text" name="username" required="required" style="height:25px;width:150px;font-size:20px;"></td>
-						</tr>
-						<tr height="40">
-							<td style="font-size:20px;">密&nbsp;&nbsp;&nbsp;&nbsp;码：</td>
-							<td><input type="password" name="password" required="required" style="height:25px;width:150px;font-size:20px;"></td>
-						</tr>
-						<tr height="40">
-							<td style="font-size:20px;">确认密码：</td>
-							<td><input type="password" name="re_password" required="required" style="height:25px;width:150px;font-size:20px;"></td>
-						</tr>
-						<tr height="40">
-							<td style="font-size:20px;">邮&nbsp;&nbsp;&nbsp;&nbsp;箱：</td>
-							<td><input type="email" name="email" required="required" style="height:25px;width:150px;font-size:20px;"></td>
+						<tr>
+							<td>用户名：</td>
+							<td><input type="text" name="username" required="required"></td>
 						</tr>
 						<tr>
-							<td align="center" style="color:red;font-size:10px;">
+							<td>密码：</td>
+							<td><input type="password" name="password" required="required"></td>
+						</tr>
+						<tr>
+							<td>确认密码：</td>
+							<td><input type="password" name="re_password" required="required"></td>
+						</tr>
+						<tr>
+							<td>邮箱：</td>
+							<td><input type="email" name="email" required="required"></td>
+						</tr>
+						<tr>
+							<td colspan="2" align="center" style="color:red;font-size:10px;">
     							<?php 
-    							     header("content-type:text/html;charset=utf-8");
     							     $err=isset($_GET["err"])?$_GET["err"]:"";
     							     switch($err){
     							         case 1:
@@ -51,13 +49,13 @@
     							?>
 							</td>
 						</tr>
-						<tr height="40">
+						<tr>
 							<td colspan="2" align="center">
-								<input type="submit" name="register" value="注册" style="font-size:20px;">
-								<input type="reset" name="reset" value="重置" style="font-size:20px;">
+								<input type="submit" name="register" value="注册">
+								<input type="reset" name="reset" value="重置">
 							</td>
 						</tr>
-						<tr height="40">
+						<tr>
 							<td colspan="2" align="center">
 								已有账号？<a href="login.php">登录</a>
 							</td>
